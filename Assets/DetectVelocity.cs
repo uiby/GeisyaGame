b11d5d56f10eb394fc3caa88d559e6699cbbbca9
Kenya <win10.kesku@gmail.com>
2016-08-23 02:18:07 +0900

    Add function calculate firing angle , initial velocity and end_y

---
+++ Assets/DetectVelocity.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectVelocity : MonoBehaviour {
+
+	//オブジェクトが衝突したとき
+  void OnTriggerStay2D(Collider2D col) {
+  	if (col.gameObject.tag == "HitPosition"){
+    	Debug.Log("速度:"+this.GetComponent<Rigidbody2D>().velocity + "  位置:" + this.transform.position);
+    }
+  }
+}
